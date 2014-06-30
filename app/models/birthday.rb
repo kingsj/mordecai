1d4243808cada3d915d77016d5236552823f3eac
Zack Chauvin <zack@iexperience.co.za>
2014-06-30 22:29:41 +0200

    added wish/birthday relation to models

--- app/models/birthday.rb
+++ app/models/birthday.rb
@@ -1,4 +1,6 @@
 class Birthday < ActiveRecord::Base
+	has_many :wishes
+
 	has_attached_file :background, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :background, :content_type => /\Aimage\/.*\Z/
 
