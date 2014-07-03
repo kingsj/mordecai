f83bdffca06edf013d2c53d34ea1ddfda05955c5
Zack Chauvin <zack@iexperience.co.za>
2014-07-03 14:53:32 +0200

    fixed wish controller

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,7 +1,7 @@
 class WishesController < ApplicationController
   def create
   	if Wish.unique_wish(params[:birthday_id], params[:wisher_id])
-  		Wish.create(message: params[:message], author: params[:author], birthday_id: params[:birthday_id], wisher_id: params[:wisher_id])
+  		@wish = Wish.create(message: params[:message], author: params[:author], birthday_id: params[:birthday_id], wisher_id: params[:wisher_id])
   		flash[:notice] = "Wish created successfully!"
   	else 
   		flash[:alert] = "Oops, looks like you've already submitted a wish!"
